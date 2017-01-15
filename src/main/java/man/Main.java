4bbf1439368c6b1a1e6281bb5b4925ce4b89a09b
xiaomin0322 <xiaomin0322@sian.com>
2017-01-15 20:38:56 +0800

    test

---
+++ src/main/java/man/Main.java
@@ -0,0 +1,17 @@
+package man;
+
+import zzm.Z;
+
+public class Main {
+
+	
+	//java -Djava.ext.dirs=D:\workspace\123\lib -jar D:\workspace\123\target\123-0.0.1-SNAPSHOT.jar
+	 //执行结果为 1，因为先假装ext的类,可用于扩展class,替换相关jar中的class
+	
+	//java -jar D:\workspace\123\target\123-0.0.1-SNAPSHOT.jar
+   //执行结果3，本地的class  
+	public static void main(String[] args) {
+		 ///ZZM.p();
+		 Z.p();
+	}
+}
