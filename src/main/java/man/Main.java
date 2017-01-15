d31e1c36fe5b502d11579898d31b9e9bd0872297
xiaomin0322 <xiaomin0322@sian.com>
2017-01-15 20:54:50 +0800

    12312

--- src/main/java/man/Main.java
+++ src/main/java/man/Main.java
@@ -5,10 +5,10 @@ import zzm.Z;
 public class Main {
 
 	
-	//java -Djava.ext.dirs=D:\workspace\123\lib -jar D:\workspace\123\target\123-0.0.1-SNAPSHOT.jar
+	//java -Djava.ext.dirs=D:\workspace\java-classload-test\lib -jar D:\workspace\java-classload-test\target\123-0.0.1-SNAPSHOT.jar
 	 //执行结果为 1，因为先假装ext的类,可用于扩展class,替换相关jar中的class
 	
-	//java -jar D:\workspace\123\target\123-0.0.1-SNAPSHOT.jar
+	//java -jar D:\workspace\java-classload-test\target\123-0.0.1-SNAPSHOT.jar
    //执行结果3，本地的class  
 	public static void main(String[] args) {
 		 ///ZZM.p();
